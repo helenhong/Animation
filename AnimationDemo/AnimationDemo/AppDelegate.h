526326c84de6727f0479b1422103d215e6295751
hongyimin <hongyimin@DCKJ-IOS-1s-Mac-mini.local>
2017-04-16 11:13:34 +0800

    init my first project in github

---
+++ AnimationDemo/AnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationDemo
+//
+//  Created by hongyimin on 2017/4/16.
+//  Copyright © 2017年 DCKJ-IOS-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationDemo/AnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationDemo
+//
+//  Created by hongyimin on 2017/4/16.
+//  Copyright © 2017年 DCKJ-IOS-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

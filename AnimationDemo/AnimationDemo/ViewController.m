95cfd1fe090fd451effcada4c22ae268c8a4dd67
hongyimin <hongyimin@DCKJ-IOS-1s-Mac-mini.local>
2017-04-18 15:21:27 +0800

    change

--- AnimationDemo/AnimationDemo/ViewController.m
+++ AnimationDemo/AnimationDemo/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *animationView;//用于表现动画的UIView
 
 @end
 
@@ -16,6 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    _animationView.layer.backgroundColor = [UIColor redColor].CGColor;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,6 +26,32 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)basicAnimationDisplay:(id)sender {
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position.y"];
+    animation.duration = 1.0f;
+    animation.toValue = @300;
+    [self.animationView.layer addAnimation:animation forKey:nil];
+}
+- (IBAction)keyFrameAnimationDisplay:(id)sender {
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"cornerRadius"];
+    animation.duration = 1.0f;
+    animation.keyTimes = @[@0,@0.3,@1];
+    animation.values = @[@0,@30,@10];
+    
+    [self.animationView.layer addAnimation:animation forKey:nil];
+}
+- (IBAction)animationGroupDisplay:(id)sender {
+    CABasicAnimation *groupmem1 = [CABasicAnimation animationWithKeyPath:@"position.y"];
+    groupmem1.toValue = @300;
+    CAKeyframeAnimation *groupmem2 = [CAKeyframeAnimation animationWithKeyPath:@"cornerRadius"];
+    groupmem2.values = @[@0,@30,@10];
+    
+    CAAnimationGroup *group = [CAAnimationGroup animation];
+    group.animations = @[groupmem1,groupmem2];
+    group.duration = 2.0f;
+    [self.animationView.layer addAnimation:group forKey:nil];
+    
 }
 
 
